20f074d8b891ab58c6f6b5c410e1b220f0b982ee
Gabriele202306 <137776159+Gabriele202306@users.noreply.github.com>
2024-01-23 09:29:55 +0100

    Add files via upload

---
+++ ConnectDB.java
@@ -0,0 +1,63 @@
+package com.gabriele.com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectDB {
+
+	public static void main(String[] args) {
+		
+		Connection myconn=null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			
+			// 1. Get a connection to a database
+			myconn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Anahata2023!");
+			if (myconn != null) {
+				System.out.println("Connection OK");
+			}else {
+				System.out.println("Connection Failed");
+			}
+		
+			// 2. Create statement
+			myStmt = myconn.createStatement();
+			String sql = "SELECT datname FROM pg_database";
+
+						
+			// 3. Execute a sql query
+			myRs = myStmt.executeQuery(sql);
+			System.out.println("List of databases:");
+		
+						
+			// 4. Process the result set
+			while (myRs.next()) {
+			String databaseName = myRs.getString("datname");
+            System.out.println(databaseName);
+		}
+		
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+            // Close resources in the reverse order of their creation
+            try {
+                if (myRs != null) {
+                    myRs.close();
+                }
+                if (myStmt != null) {
+                    myStmt.close();
+                }
+                if (myconn != null) {
+                    myconn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
